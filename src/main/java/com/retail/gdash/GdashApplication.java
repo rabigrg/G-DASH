660311861316997a6b96cf9a2e6dfa6e312d8e36
Rabi Gurung <44107376+rabigrg@users.noreply.github.com>
2021-04-06 14:37:11 -0500

    initial project setup

---
+++ src/main/java/com/retail/gdash/GdashApplication.java
@@ -0,0 +1,13 @@
+package com.retail.gdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retail/gdash/GdashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.gdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
